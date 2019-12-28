a8682d4456b3411cc422949a44f95d0aab43d144
Derek Gusoff <dgusoff@rightpoint.com>
2019-12-27 23:11:22 -0500

    first chart working

--- src/components/LineCharts/SimpleLineChart.tsx
+++ src/components/LineCharts/SimpleLineChart.tsx
@@ -1,9 +1,60 @@
 import * as React from "react";
+import Chart from 'chart.js';
 
-export class SimpleLineChart extends React.Component {
-    public render(){
-        return(
-            <p>hello</p>
-        )
+export class SimpleLineChart extends React.Component<{}, {}> {
+    node: any;
+
+    constructor(props: Readonly<{}>) {
+        super(props);
+        this.node = this.refs.canvas as HTMLCanvasElement
     }
+
+    componentDidMount() {
+        console.log(this.refs);
+        const canvas = this.refs.myCanvas as HTMLCanvasElement;
+
+        if (canvas && canvas != null) {
+            console.log(canvas);
+            const ctx = canvas.getContext("2d");
+            if (ctx != null) {
+                console.log(ctx);
+                new Chart(ctx, {
+                    type: "line",
+                    data: {
+                        //Bring in data
+                        labels: ["Jan", "Feb", "March"],
+                        datasets: [
+                            {
+                                label: "Sales",
+                                data: [86, 67, 91],
+                            }
+                        ]
+                    },
+                    options: {
+                        //Customize chart options
+                    }
+                });
+        }
+
+    }
+
+}
+
+    public render() {
+
+
+    return (
+        <div >
+            <p>simple line chart</p>
+            <canvas
+                id="myChart"
+                ref="myCanvas"
+            />
+
+        </div>
+    )
+
+
+
+}
 }
