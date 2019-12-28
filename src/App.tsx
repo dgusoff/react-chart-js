3c4a55d4a333277498e7ca7977e20920f766c419
Derek Gusoff <dgusoff@rightpoint.com>
2019-12-27 20:38:19 -0500

    add scss file

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './scss/app.scss';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
+    <div className="app">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+       
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
