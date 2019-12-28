31a7b0e6a4a78efe83ebfae401a04ccb70735751
Derek Gusoff <dgusoff@rightpoint.com>
2019-12-27 23:39:24 -0500

    clean up some cruft

--- src/components/LineCharts/SimpleLineChart.tsx
+++ src/components/LineCharts/SimpleLineChart.tsx
@@ -1,60 +1,42 @@
 import * as React from "react";
 import Chart from 'chart.js';
 
-export class SimpleLineChart extends React.Component<{}, {}> {
-    node: any;
-
-    constructor(props: Readonly<{}>) {
-        super(props);
-        this.node = this.refs.canvas as HTMLCanvasElement
-    }
+export class SimpleLineChart extends React.Component {
 
     componentDidMount() {
-        console.log(this.refs);
         const canvas = this.refs.myCanvas as HTMLCanvasElement;
 
-        if (canvas && canvas != null) {
-            console.log(canvas);
-            const ctx = canvas.getContext("2d");
-            if (ctx != null) {
-                console.log(ctx);
-                new Chart(ctx, {
-                    type: "line",
-                    data: {
-                        //Bring in data
-                        labels: ["Jan", "Feb", "March"],
-                        datasets: [
-                            {
-                                label: "Sales",
-                                data: [86, 67, 91],
-                            }
-                        ]
-                    },
-                    options: {
-                        //Customize chart options
-                    }
-                });
+        const ctx = canvas.getContext("2d");
+        if (ctx != null) {
+            new Chart(ctx, {
+                type: "line",
+                data: {
+                    labels: ["Monday", "Tuesday", "Wednesday", "Thursday"],
+                    datasets: [
+                        {
+                            label: "High Temperature",
+                            data: [44, 32, 51, 17],
+                        },
+                        {
+                            label: "Low Temperature",
+                            data: [21, 10, 22, -1],
+                        }
+                    ]
+                }
+            });
         }
-
     }
 
-}
 
     public render() {
-
-
-    return (
-        <div >
-            <p>simple line chart</p>
-            <canvas
-                id="myChart"
-                ref="myCanvas"
-            />
-
-        </div>
-    )
-
-
-
-}
+        return (
+            <div>
+                <p>simple line chart</p>
+                <canvas
+                    id="myChart"
+                    ref="myCanvas"
+                />
+            </div>
+        )
+    }
 }
